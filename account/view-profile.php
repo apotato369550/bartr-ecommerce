f34dfe35c614595e3ea3df4582b4958b25f08c62
apotato369550 <johnandresyap510@gmail.com>
2021-06-29 19:02:22 +0800

    Work on profile page

--- account/view-profile.php
+++ account/view-profile.php
@@ -33,31 +33,42 @@ if(empty($row["profile_picture"])){
 // work on thisVVV
 // copy format from login and register fields but w/ secondary color instead of dark
 // figure out the ting
+// work on this
+// copy first the signup ting
+// is there a .css file here?
 ?>
 
-<div class="text-center jumbotron mx-auto text-white bg-secondary jumbotron">
+<main>
+	<div class="text-center jumbotron mx-auto text-white bg-secondary user-profile">
 
-	<h1 class="display-6">Your Profile: </h1>
-	<img width="200" height="200" alt="<?php echo $profilePic ?>" src="<?php echo $profilePic ?>">
-
-	<div>
-		<p>Userame: <?php echo $username; ?> </p>
-		<p>Email: <?php echo $email; ?> </p>
-
-		<h1>Settings: </h1>
-
-		<button onclick="document.getElementById('change-profile-pic-form-container').style.display = 'block';">Edit Profile Pic</button>
-		<div id="change-profile-pic-form-container" style="display: none;">
-			<form method="POST" action="includes/change-profile-pic.inc.php" enctype="multipart/form-data">
-				<input type="hidden" name="username" value="<?php echo $_SESSION["username"]; ?>">
-				<input type="file" name="image">
-				<button type="submit" name="submit">Edit</button>
-			</form>
+		<h1 class="display-4">Your Profile: </h1>
+		<div>
+			<img width="200" height="200" alt="<?php echo $profilePic ?>" src="<?php echo $profilePic ?>" class="rounded-circle">
+			<button onclick="document.getElementById('change-profile-pic-form-container').style.display = 'block';">Edit Profile Pic</button>
+			<div id="change-profile-pic-form-container" style="display: none;">
+				<form method="POST" action="includes/change-profile-pic.inc.php" enctype="multipart/form-data">
+					<input type="hidden" name="username" value="<?php echo $_SESSION["username"]; ?>">
+					<input type="file" name="image">
+					<button type="submit" name="submit">Edit</button>
+				</form>
+			</div>
 		</div>
-		<a href="user/email-change-request.php">Change Email</a>
-		<a href="user/reset-password.php">Reset Password</a>
-		<a href="user/delete-request.php">Delete Account</a>
+		<hr class="light">
+
+		<div>
+			<p>Userame: <?php echo $username; ?> </p>
+			<p>Email: <?php echo $email; ?> </p>
+
+			<h1>Settings: </h1>
+
+			
+			<a href="user/email-change-request.php">Change Email</a>
+			<a href="user/reset-password.php">Reset Password</a>
+			<a href="user/delete-request.php">Delete Account</a>
+		</div>
 	</div>
-</div>
+</main>
+
+
 
 
